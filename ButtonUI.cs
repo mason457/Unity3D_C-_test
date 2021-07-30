fe7a83f9c47cf2d676134f2ff9f3d4518260a152
mason457 <88136451+mason457@users.noreply.github.com>
2021-07-30 18:28:23 +0800

    Add files via upload

---
+++ ButtonUI.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;  //引用場景管理功能
+
+
+public class ButtonUI : MonoBehaviour
+{
+  public GameObject Obj;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ClickExit()
+    {
+        Application.Quit();
+    }
+
+     public void ClickEnd1()
+    {
+        SceneManager.LoadScene("JB");
+        Time.timeScale = 1;
+    }
+
+    public void ClickRestart1()
+    {
+        SceneManager.LoadScene("JB");
+        Time.timeScale = 1;
+    }
+    public void ClickStart()
+    {
+        Obj.SetActive(false);
+        Time.timeScale = 1;
+    }
+     
+}
