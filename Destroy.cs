ed434db71f5f5d634cee83a5a7379fb1569a40ef
mason457 <88136451+mason457@users.noreply.github.com>
2021-07-30 18:25:28 +0800

    Add files via upload

---
+++ Destroy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    public float timelimit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, timelimit); // 幾秒後⾃我銷毀
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ POINT.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class POINT : MonoBehaviour
+{
+    int m_timer;
+    public Text M_value;
+    public Text Time_value;
+    public int SA; 
+    float STime;
+    int point;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        STime += Time.deltaTime;
+        point = (int)STime * SA;
+        M_value.text = string.Format("分數:{0}",point.ToString(""));
+        Time_value.text = string.Format("時間:{0}",STime.ToString(""));
+        //M_value.text = SA;
+    }
+}
